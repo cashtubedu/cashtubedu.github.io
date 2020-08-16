53bcf1a3915f6c92cae600915ea1bcc3149dd70e
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-08-16 22:12:27 +0530

    Second

---
+++ precache-manifest.8626264331ebb9255d66361a156a9708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263a78cd96b1a1cbe1ded8518cc97873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "273746e9e924296112f3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5afa47aab958703c3557",
+    "url": "/static/js/2.b3820c4c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b3820c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273746e9e924296112f3",
+    "url": "/static/js/main.e9bcbc0d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
